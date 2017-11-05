a5677c1c6abeee17e9d843a862b204a6159dc210
Laurenz Seidel <LaurenzSeidel@gmail.com>
2017-11-05 23:14:25 +0100

    Added author-model and respective view. Closes #3.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,7 +2,19 @@ class AuthorsController < ApplicationController
     def new
     end
 
+    def show
+        @author = Author.find(params[:id])
+    end
+
     def create
-        render plain: params[:author].inspect
+        @author = Author.new(author_params)
+
+        @author.save
+        redirect_to @author
+    end
+
+    private
+    def author_params
+        params.require(:author).permit(:first_name, :last_name, :website)
     end
 end
--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -9,8 +9,8 @@ describe "New author page", type: :feature do
     it "should have text input for first name, last name, and homepage" do
         visit new_author_path
 
-        expect(page).to have_field('First name')
-        expect(page).to have_field('Last name')
-        expect(page).to have_field('Homepage')
+        expect(page).to have_field('author[first_name]')
+        expect(page).to have_field('author[last_name]')
+        expect(page).to have_field('author[website]')
     end
 end
