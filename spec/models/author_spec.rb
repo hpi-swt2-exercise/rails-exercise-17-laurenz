1f2be2f798dc614d1d3eb49fd48240185e89a213
Laurenz Seidel <LaurenzSeidel@gmail.com>
2017-11-05 20:34:10 +0100

    Failing test for #3.

---
+++ spec/models/author_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "Author model", type: :model do
+
+    it "should get created with correct data" do
+        author = Author.new("Alan", "Turing", "http://wikipedia.org/Alan_Turing")
+
+        expect(author.first_name).to eq("Alan")
+        expect(author.last_name).to eq("Turing")
+        expect(author.website).to eq("http://wikipedia.org/Alan_Turing")
+    end
+
+end
