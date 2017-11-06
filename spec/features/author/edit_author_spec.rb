8648770a77831ccf6468d91818ac6e28fafaed51
Laurenz Seidel <LaurenzSeidel@gmail.com>
2017-11-06 18:17:26 +0100

    added test for #16

---
+++ spec/features/author/edit_author_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "Edit author page", type: :feature do
+
+    it "should save changes" do
+        author = FactoryGirl.create :author
+        visit edit_author_path(author)
+
+        fill_in 'Last name', with: 'Torengo'
+        click_button 'Save Author'
+    end
+
+end
