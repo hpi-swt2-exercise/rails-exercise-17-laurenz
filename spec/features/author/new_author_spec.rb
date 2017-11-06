97f9ee2683ca1e922a45d07be9ef16d074429067
Laurenz Seidel <LaurenzSeidel@gmail.com>
2017-11-06 15:15:22 +0100

    add failing test for #14

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -13,4 +13,27 @@ describe "New author page", type: :feature do
         expect(page).to have_field('Last name')
         expect(page).to have_field('Homepage')
     end
+
+    it "should show validation error" do
+        visit new_author_path
+
+        fill_in 'First name', with: 'Alan'
+        fill_in 'Homepage', with: 'http://wikipedia.de/Alan_Turing'
+
+        click_button 'Save Author'
+
+        expect(page).to have_content("last name can't be blank")
+    end
+
+    it "should accept when all data is provided" do
+        visit new_author_path
+
+        fill_in 'First name', with: 'Alan'
+        fill_in 'Homepage', with: 'http://wikipedia.de/Alan_Turing'
+        fill_in 'Last name', with: 'Turing'
+
+        click_button 'Save Author'
+
+        expect(page).to have_no_content("last name can't be blank")
+    end
 end
