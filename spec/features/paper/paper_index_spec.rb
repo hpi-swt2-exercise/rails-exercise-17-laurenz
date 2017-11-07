ed81fe86e788c5cd0680537d1d2e92abe4cdf8ae
Laurenz Seidel <LaurenzSeidel@gmail.com>
2017-11-07 23:39:15 +0100

    added failing test for #26

--- spec/features/paper/paper_index_spec.rb
+++ spec/features/paper/paper_index_spec.rb
@@ -5,7 +5,7 @@ describe "Paper index page", type: :feature do
     it "should render" do
         visit papers_path
     end
-    
+
     it "should list title, venue, and year of papers" do
         paper = FactoryGirl.create :paper
         visit papers_path
@@ -13,6 +13,13 @@ describe "Paper index page", type: :feature do
         expect(page).to have_content(paper.title)
         expect(page).to have_content(paper.venue)
         expect(page).to have_content(paper.year)
+    end
+
+    it "should link to paper page" do
+        paper = FactoryGirl.create :paper
+        visit papers_path
+
+        expect(page).to have_link("Show")
     end
 
 end
