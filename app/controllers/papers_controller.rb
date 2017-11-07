d427bdfceac0c1f2fd4fb5a0e98416a7406e22f8
Laurenz Seidel <LaurenzSeidel@gmail.com>
2017-11-07 20:23:57 +0100

    added new paper page. closes #18

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -1,2 +1,26 @@
 class PapersController < ApplicationController
+
+    def show
+        @paper = Paper.find(params[:id])
+    end
+
+    def new
+        @paper = Paper.new
+    end
+
+    def create
+        @paper = Paper.new(paper_params)
+
+        if @paper.save
+            redirect_to @paper
+        else
+            render 'new'
+        end
+    end
+
+    private
+    def paper_params
+        params.require(:author).permit(:title, :venue, :year)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'home#index'
-  
-  resources :authors
- 
+
+  resources :authors do
+      resources :papers
+  end
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
