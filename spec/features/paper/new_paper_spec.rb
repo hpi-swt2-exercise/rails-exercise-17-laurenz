248c72113710b02bfaf09050735fd3806ea5ba74
Laurenz Seidel <LaurenzSeidel@gmail.com>
2017-11-07 20:38:04 +0100

    added failing test for #20

--- spec/features/paper/new_paper_spec.rb
+++ spec/features/paper/new_paper_spec.rb
@@ -6,4 +6,15 @@ describe "New author page", type: :feature do
         visit new_paper_path
     end
 
+    it "should show validation error" do
+        visit new_paper_path
+
+        fill_in 'Venue', with: 'Evergreen Terrace'
+        fill_in 'Year', with: '1928'
+
+        click_button 'Save Paper'
+
+        expect(page).to have_content("Title can't be blank")
+    end
+
 end
