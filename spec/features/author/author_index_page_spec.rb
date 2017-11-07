b8025b400410a4021103bcc84c8d3761a102c212
Laurenz Seidel <LaurenzSeidel@gmail.com>
2017-11-07 19:07:06 +0100

    added test for #17"

--- spec/features/author/author_index_page_spec.rb
+++ spec/features/author/author_index_page_spec.rb
@@ -35,5 +35,17 @@ describe "Author index page", type: :feature do
         expect(page).to have_link("Edit")
     end
 
+    it "should support deleting authors" do
+        author = FactoryGirl.create :author
+        visit authors_path
+
+        click_link "Destroy"
+
+        visit authors_path
+        expect(page).to have_no_content(author.first_name)
+        expect(page).to have_no_content(author.last_name)
+        expect(page).to have_no_content(author.homepage)
+    end
+
 
 end
