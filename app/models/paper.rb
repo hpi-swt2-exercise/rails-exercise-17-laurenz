1e3c959f6ea4c7c859d8ffd5da914addb6ccccad
Laurenz Seidel <LaurenzSeidel@gmail.com>
2017-11-08 14:50:54 +0100

    added one-to-many relationship

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,5 +1,5 @@
 class Paper < ActiveRecord::Base
-  belongs_to :author
+  has_and_belongs_to_many :authors
   validates :title, :venue, :year, presence: true
   validates :year, numericality: { only_integer: true }
 end
