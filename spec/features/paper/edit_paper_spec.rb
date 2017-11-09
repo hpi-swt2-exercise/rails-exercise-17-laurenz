bdd5b2705ccf9ab725273253b78c6839291d0915
Laurenz Seidel <LaurenzSeidel@gmail.com>
2017-11-09 20:14:35 +0100

    added failing test for #32

--- spec/features/paper/edit_paper_spec.rb
+++ spec/features/paper/edit_paper_spec.rb
@@ -19,7 +19,16 @@ describe "Edit paper page", type: :feature do
         expect(page).to have_field("Author 3")
         expect(page).to have_field("Author 4")
         expect(page).to have_field("Author 5")
+    end
 
+    it "should allow to select 5 authors from 5 separate drop downs" do
+        author = FactoryGirl.create :author
+        paper = FactoryGirl.create :paper
+        visit edit_paper_path(paper)
+
+        select author.name, from: 'Author 1'
+        expect(paper.authors[0]).to eq(author)
+        #equals might not work, switch to name comparison then
     end
 
 end
