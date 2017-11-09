b84dad73d0b3d7b7b836670dd8aad4d93788eb77
Laurenz Seidel <LaurenzSeidel@gmail.com>
2017-11-09 20:08:30 +0100

    added test fpr #31

--- spec/features/paper/edit_paper_spec.rb
+++ spec/features/paper/edit_paper_spec.rb
@@ -10,4 +10,16 @@ describe "Edit paper page", type: :feature do
         click_button 'Update Paper'
     end
 
+    it "should allow to select 5 authors from 5 separate drop downs" do
+        paper = FactoryGirl.create :paper
+        visit edit_paper_path(paper)
+
+        expect(page).to have_field("Author 1")
+        expect(page).to have_field("Author 2")
+        expect(page).to have_field("Author 3")
+        expect(page).to have_field("Author 4")
+        expect(page).to have_field("Author 5")
+
+    end
+
 end
